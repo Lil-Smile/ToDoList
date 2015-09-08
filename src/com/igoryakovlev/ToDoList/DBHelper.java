88bda4fba2291f1ff6d3791fa8f02ce48e22453b
Igor <i.got.that.flower@gmail.com>
2015-09-08 13:01:27 +0300

    Added adding todo

--- src/com/igoryakovlev/ToDoList/DBHelper.java
+++ src/com/igoryakovlev/ToDoList/DBHelper.java
@@ -1,6 +1,8 @@
 package com.igoryakovlev.ToDoList;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -10,6 +12,9 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DBHelper extends SQLiteOpenHelper {
 
     private static int VERSION = 1;
+    private static String TABLE_NAME = "to_do_table";
+    public static String JOBS_NAME = "jobs_name";
+    public static String DONE_OR_NOT = "done_or_not";
 
     public DBHelper(Context context)
     {
@@ -17,17 +22,69 @@ public class DBHelper extends SQLiteOpenHelper {
     }
 
 
-    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
-    }
+
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        String executionString = "CREATE TABLE "+TABLE_NAME+" (id integer autoincrement, "+JOBS_NAME+" text, "+DONE_OR_NOT+" text);";
+        db.execSQL(executionString);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
+
+    public Cursor getAll()
+    {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(TABLE_NAME,null,null,null,null,null,null,null);
+        return cursor;
+    }
+
+    public Cursor getDone()
+    {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String selection = DONE_OR_NOT+" like ? ";
+        String[] selectionArgs = {"true"};
+        Cursor cursor = db.query(TABLE_NAME,null,selection,selectionArgs,null,null,null);
+        return cursor;
+    }
+
+    public Cursor getNotDone()
+    {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String selection = DONE_OR_NOT+" like ? ";
+        String[] selectionArgs = {"false"};
+        Cursor cursor = db.query(TABLE_NAME,null,selection,selectionArgs,null,null,null);
+        return cursor;
+    }
+
+
+    public void writeToDB(String todo)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(JOBS_NAME,todo);
+        cv.put(DONE_OR_NOT,"false");
+        db.insert(TABLE_NAME, null, cv);
+    }
+
+    public void updateDb(String todo, boolean done)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(JOBS_NAME,todo);
+        if (done)
+        {
+            cv.put(DONE_OR_NOT, "true");
+        } else
+        {
+            cv.put(DONE_OR_NOT, "false");
+        }
+        String[] args = {todo};
+        db.update(TABLE_NAME,cv,JOBS_NAME+" like ? ",args);
+    }
+
+
 }
--- src/com/igoryakovlev/ToDoList/MainActivity.java
+++ src/com/igoryakovlev/ToDoList/MainActivity.java
@@ -1,10 +1,20 @@
 package com.igoryakovlev.ToDoList;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends Activity implements View.OnClickListener{
     /**
@@ -17,6 +27,13 @@ public class MainActivity extends Activity implements View.OnClickListener{
     Button buttonNotDone;
 
     ListView lvListofJobs;
+
+    DBHelper dbHelper;
+
+    String[] from = {DBHelper.DONE_OR_NOT,DBHelper.JOBS_NAME};
+    int[] to = {R.id.cbJob, R.id.tvJob};
+
+    ArrayList<HashMap<String,Object>> data = new ArrayList<HashMap<String, Object>>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,6 +51,28 @@ public class MainActivity extends Activity implements View.OnClickListener{
         buttonAll.setOnClickListener(this);
         buttonAdd.setOnClickListener(this);
 
+        dbHelper = new DBHelper(this);
+        Cursor cursor = dbHelper.getAll();
+        if (cursor.moveToFirst())
+        {
+            int jobColIndex = cursor.getColumnIndex(DBHelper.JOBS_NAME);
+            int doneColIndex = cursor.getColumnIndex(DBHelper.DONE_OR_NOT);
+            do
+            {
+                HashMap<String,Object> m = new HashMap<String, Object>();
+                m.put(DBHelper.JOBS_NAME,cursor.getString(jobColIndex));
+                boolean tmp = false;
+                if (cursor.getString(doneColIndex)=="true")
+                {
+                    tmp = true;
+                }
+                m.put(DBHelper.DONE_OR_NOT,tmp);
+                data.add(m);
+            } while (cursor.moveToNext());
+        }
+
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this,data,R.layout.view_for_list,from,to);
+        lvListofJobs.setAdapter(simpleAdapter);
     }
 
     @Override
@@ -43,18 +82,129 @@ public class MainActivity extends Activity implements View.OnClickListener{
         {
             case R.id.buttonAdd:
             {
+                final Dialog dialog = new Dialog(this);
+                dialog.setContentView(R.layout.add_todo_dialog);
+                dialog.setTitle(getString(R.string.newTask));
+                Button buttonOk = (Button)dialog.findViewById(R.id.buttonOK);
+                Button buttonCancel = (Button)dialog.findViewById(R.id.buttonCancel);
+                buttonOk.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        EditText editText = (EditText)dialog.findViewById(R.id.etNewToDo);
+                        String todo = editText.getText().toString();
+                        dbHelper.writeToDB(todo);
+                        ArrayList<HashMap<String,Object>> tmpData = new ArrayList<HashMap<String, Object>>();
+                        Cursor cursor = dbHelper.getAll();
+                        if (cursor.moveToFirst())
+                        {
+                            int jobColIndex = cursor.getColumnIndex(DBHelper.JOBS_NAME);
+                            int doneColIndex = cursor.getColumnIndex(DBHelper.DONE_OR_NOT);
+                            do
+                            {
+                                HashMap<String,Object> m = new HashMap<String, Object>();
+                                m.put(DBHelper.JOBS_NAME,cursor.getString(jobColIndex));
+                                boolean tmp = false;
+                                if (cursor.getString(doneColIndex)=="true")
+                                {
+                                    tmp = true;
+                                }
+                                m.put(DBHelper.DONE_OR_NOT,tmp);
+                                tmpData.add(m);
+                            } while (cursor.moveToNext());
+                        }
+
+                        SimpleAdapter simpleAdapter = new SimpleAdapter(getApplicationContext(),tmpData,R.layout.view_for_list,from,to);
+                        lvListofJobs.setAdapter(simpleAdapter);
+                        dialog.dismiss();
+
+                    }
+                });
+                buttonCancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+
                 break;
             }
             case R.id.buttonAll:
             {
+                ArrayList<HashMap<String,Object>> dataTmp = new ArrayList<HashMap<String, Object>>();
+                Cursor cursor = dbHelper.getAll();
+                if (cursor.moveToFirst())
+                {
+                    int jobColIndex = cursor.getColumnIndex(DBHelper.JOBS_NAME);
+                    int doneColIndex = cursor.getColumnIndex(DBHelper.DONE_OR_NOT);
+                    do
+                    {
+                        HashMap<String,Object> m = new HashMap<String, Object>();
+                        m.put(DBHelper.JOBS_NAME,cursor.getString(jobColIndex));
+                        boolean tmp = false;
+                        if (cursor.getString(doneColIndex)=="true")
+                        {
+                            tmp = true;
+                        }
+                        m.put(DBHelper.DONE_OR_NOT,tmp);
+                        dataTmp.add(m);
+                    } while (cursor.moveToNext());
+                }
+
+                SimpleAdapter simpleAdapter = new SimpleAdapter(this,dataTmp,R.layout.view_for_list,from,to);
+                lvListofJobs.setAdapter(simpleAdapter);
                 break;
             }
             case R.id.buttonDone:
             {
+                ArrayList<HashMap<String,Object>> dataTmp = new ArrayList<HashMap<String, Object>>();
+                Cursor cursor = dbHelper.getDone();
+                if (cursor.moveToFirst())
+                {
+                    int jobColIndex = cursor.getColumnIndex(DBHelper.JOBS_NAME);
+                    int doneColIndex = cursor.getColumnIndex(DBHelper.DONE_OR_NOT);
+                    do
+                    {
+                        HashMap<String,Object> m = new HashMap<String, Object>();
+                        m.put(DBHelper.JOBS_NAME,cursor.getString(jobColIndex));
+                        boolean tmp = false;
+                        if (cursor.getString(doneColIndex)=="true")
+                        {
+                            tmp = true;
+                        }
+                        m.put(DBHelper.DONE_OR_NOT,tmp);
+                        dataTmp.add(m);
+                    } while (cursor.moveToNext());
+                }
+
+                SimpleAdapter simpleAdapter = new SimpleAdapter(this,dataTmp,R.layout.view_for_list,from,to);
+                lvListofJobs.setAdapter(simpleAdapter);
                 break;
             }
             case R.id.buttonNotDone:
             {
+                ArrayList<HashMap<String,Object>> dataTmp = new ArrayList<HashMap<String, Object>>();
+                Cursor cursor = dbHelper.getNotDone();
+                if (cursor.moveToFirst())
+                {
+                    int jobColIndex = cursor.getColumnIndex(DBHelper.JOBS_NAME);
+                    int doneColIndex = cursor.getColumnIndex(DBHelper.DONE_OR_NOT);
+                    do
+                    {
+                        HashMap<String,Object> m = new HashMap<String, Object>();
+                        m.put(DBHelper.JOBS_NAME,cursor.getString(jobColIndex));
+                        boolean tmp = false;
+                        if (cursor.getString(doneColIndex)=="true")
+                        {
+                            tmp = true;
+                        }
+                        m.put(DBHelper.DONE_OR_NOT,tmp);
+                        dataTmp.add(m);
+                    } while (cursor.moveToNext());
+                }
+
+                SimpleAdapter simpleAdapter = new SimpleAdapter(this,dataTmp,R.layout.view_for_list,from,to);
+                lvListofJobs.setAdapter(simpleAdapter);
                 break;
             }
         }
